e50f90acb995a43519e9248cf4d75f7d9810bffe
lgm0905 <lgm0905@snu.ac.kr>
2020-10-27 21:41:57 +0900

    Delete db

--- backend/stockin/apps/users/managers.py
+++ backend/stockin/apps/users/managers.py
@@ -7,14 +7,14 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, nickname, password, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, nickname=nickname, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
