4a03613994aa6deaf92ff86e5a143c15a6c2dbca
Wooyoung Choi <w980417@gmail.com>
2020-10-30 03:50:01 +0900

    add tabs in detailpage

--- stockin/frontend/src/containers/DetailPage/DetailPage.js
+++ stockin/frontend/src/containers/DetailPage/DetailPage.js
@@ -7,54 +7,38 @@ import DetailPriceTrends from '../../components/Detail/DetailPriceTrends/DetailP
 import DetailFinancialState from '../../components/Detail/DetailFinancialState/DetailFinancialState';
 import DetailComment from '../../components/Detail/DetailComment/DetailComment';
 import Footer from '../../components/Footer/Footer';
+import { Tab } from 'semantic-ui-react';
+
+const panes = [
+  {
+    menuItem: 'Overview',
+    render: () => <DetailOverview />,
+  },
+  {
+    menuItem: 'News',
+    render: () => <DetailNews />,
+  },
+  {
+    menuItem: 'Price Trends',
+    render: () => <DetailPriceTrends />,
+  },
+  {
+    menuItem: 'Financial State',
+    render: () => <DetailFinancialState />,
+  },
+  {
+    menuItem: 'Comments',
+    render: () => <DetailComment />,
+  },
+];
 
 const DetailPage = (props) => {
-  const [value, setValue] = React.useState(2);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  let currentTab = 'dddd';
-  switch (value) {
-    case 0:
-      currentTab = <DetailOverview />;
-      break;
-    case 1:
-      currentTab = <DetailNews />;
-      break;
-    case 2:
-      currentTab = <DetailPriceTrends />;
-      break;
-    case 3:
-      currentTab = <DetailFinancialState />;
-      break;
-    case 4:
-      currentTab = <DetailComment />;
-      break;
-
-    default:
-      break;
-  }
-
   return (
     <div className="DetailPage">
       <Header history={props.history} />
       <DetailData name={props.name} code={props.code} />
-      {/* <Tabs
-        value={value}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChange}
-        centered
-      >
-        <Tab label="Overview" />
-        <Tab label="News" />
-        <Tab label="Price Trends" />
-        <Tab label="Financial state" />
-        <Tab label="Comments" />
-      </Tabs> */}
-      {currentTab}
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+
       <Footer history={props.history} />
     </div>
   );
