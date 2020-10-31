c617dec3aedfb861551be1ed31f1889f267ac5d1
Wooyoung Choi <w980417@gmail.com>
2020-10-31 14:51:26 +0900

    add test in Header.js

--- stockin/frontend/src/components/Header/Header.test.js
+++ stockin/frontend/src/components/Header/Header.test.js
@@ -13,10 +13,18 @@ describe('<Header />', () => {
     expect(query.length).toBe(1);
   });
 
+  it('should call onClickNavHandler when clicking Logo', () => {
+    const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
+    const { container } = render(<Header history={history} />);
+    const button = getByTestId(container, 'Logo');
+    fireEvent.click(button);
+    expect(spyHistoryPush).toHaveBeenCalledTimes(1);
+  });
+
   it('should call onClickNavHandler when clicking REPORT', () => {
     const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
     const { container } = render(<Header history={history} />);
-    const button = getByTestId(container, 'REPORT');
+    const button = getByTestId(container, 'Report');
     fireEvent.click(button);
     expect(spyHistoryPush).toHaveBeenCalledTimes(1);
   });
@@ -24,7 +32,7 @@ describe('<Header />', () => {
   it('should call onClickNavHandler when clicking MY PAGE', () => {
     const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
     const { container } = render(<Header history={history} />);
-    const button = getByTestId(container, 'MY PAGE');
+    const button = getByTestId(container, 'Mypage');
     fireEvent.click(button);
     expect(spyHistoryPush).toHaveBeenCalledTimes(1);
   });
@@ -32,7 +40,31 @@ describe('<Header />', () => {
   it('should call onClickNavHandler when clicking ABOUT US', () => {
     const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
     const { container } = render(<Header history={history} />);
-    const button = getByTestId(container, 'ABOUT US');
+    const button = getByTestId(container, 'AboutUs');
+    fireEvent.click(button);
+    expect(spyHistoryPush).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onClickNavHandler when clicking Logo in StickyHeader', () => {
+    const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
+    const { container } = render(<Header history={history} />);
+    const button = getByTestId(container, 'stickyLogo');
+    fireEvent.click(button);
+    expect(spyHistoryPush).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onClickNavHandler when clicking REPORT in StickyHeader', () => {
+    const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
+    const { container } = render(<Header history={history} />);
+    const button = getByTestId(container, 'stickyReport');
+    fireEvent.click(button);
+    expect(spyHistoryPush).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onClickNavHandler when clicking MY PAGE in StickyHeader', () => {
+    const spyHistoryPush = jest.spyOn(history, 'push').mockImplementation((text) => true);
+    const { container } = render(<Header history={history} />);
+    const button = getByTestId(container, 'stickyMypage');
     fireEvent.click(button);
     expect(spyHistoryPush).toHaveBeenCalledTimes(1);
   });
