8bd1d30f8365843ee67a995cea58573f4e18c162
Junhyeok Park <dillonzzang@gmail.com>
2020-11-01 05:16:48 +0900

    added custom error 406 for duplicate user

--- stockin/backend/apps/groups/urls.py
+++ stockin/backend/apps/groups/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from groups import views
 
 urlpatterns = [
-    path('create', views.group_create, name='group_create'),
-    path('edit/<int:id>', views.group_edit, name='group_edit'),
-    path('list', views.group_list, name='group_list'),
-    path('<int:id>/stocks', views.group_stock_list, name='group_stock edit'),
+    path('create/', views.group_create, name='group_create'),
+    path('edit/<int:id>/', views.group_edit, name='group_edit'),
+    path('list/', views.group_list, name='group_list'),
+    path('<int:id>/stocks/', views.group_stock_list, name='group_stock edit'),
 ]
