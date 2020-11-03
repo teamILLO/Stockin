f93a395f4d142e111c82e825a07d91ffa4df7656
Junhyeok Park <dillonzzang@gmail.com>
2020-11-04 00:35:38 +0900

    added cors header settins and comment model

--- stockin/backend/apps/users/urls.py
+++ stockin/backend/apps/users/urls.py
@@ -3,8 +3,8 @@ from users import views
 
 urlpatterns = [
     # user model
-    path('signup', views.signup, name='sign_up'),
-    path('signin', views.signin, name='sign_in'),
-    path('signout', views.signout, name='sign_out'),
-    path('token', views.token, name='token'),
+    path('signup/', views.signup, name='sign_up'),
+    path('signin/', views.signin, name='sign_in'),
+    path('signout/', views.signout, name='sign_out'),
+    path('token/', views.token, name='token'),
 ]
