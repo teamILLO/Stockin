b8bcf76500b463526cea7b530bc648b42ac7a3a8
lgm0905 <lgm0905@snu.ac.kr>
2020-11-06 01:43:20 +0900

    Add recent search

--- stockin/frontend/src/components/SearchBox/SearchBox.js
+++ stockin/frontend/src/components/SearchBox/SearchBox.js
@@ -8,7 +8,7 @@ import { getStocks } from '../../store/stock';
 
 const initialState = {
   loading: false,
-  results: [],
+  results: JSON.parse(localStorage.getItem('recent-search')) || [],
   value: '',
   stocks: [],
 };
@@ -20,7 +20,7 @@ function exampleReducer(state, action) {
     case 'START_SEARCH':
       return { ...state, loading: true, value: action.query };
     case 'FINISH_SEARCH':
-      return { ...state, loading: false, results: action.results };
+      return { ...state, loading: false, results: state.results.concat(action.results) };
     case 'UPDATE_SELECTION':
       return { ...state, value: action.selection };
 
@@ -31,13 +31,14 @@ function exampleReducer(state, action) {
 
 const resultRenderer = ({ title }) => <p>{title}</p>;
 
+const keyGenerator = () => '_' + Math.random().toString(36).substr(2, 9);
+
 function SearchBox() {
   const history = useHistory(); 
   const _dispatch = useDispatch();
   const { stockList } = useSelector((state) => state.stock);
   const [state, dispatch] = React.useReducer(exampleReducer, initialState);
   const { loading, results, value } = state;
-
   const timeoutRef = React.useRef();
 
   React.useEffect(() => {
@@ -48,27 +49,44 @@ function SearchBox() {
   }, []);
 
   const handleSearchChange = React.useCallback((e, data) => {
-    console.log(data)
     clearTimeout(timeoutRef.current);
     dispatch({ type: 'START_SEARCH', query: data.value });
-
 
     timeoutRef.current = setTimeout(() => {
       if (data.value.length === 0) {
         dispatch({ type: 'CLEAN_QUERY' });
         return;
       }
-      dispatch({
-        type: 'FINISH_SEARCH',
-        results: stockList.filter((st) => st.title.toUpperCase().includes(data.value.toUpperCase())),
-      });
+
+      var search_result = [];
+      var _search_result = stockList.filter((st) => st.title.toUpperCase().includes(data.value.toUpperCase())).slice(0,5);
+      var i;
+      for(i = 0; i < _search_result.length; i++) {
+        var temp = {..._search_result[i], key : keyGenerator()};
+        search_result.push(temp);
+      }
+
+      console.log(search_result);
+
+      dispatch({type: 'FINISH_SEARCH', results: search_result});
     }, 300);
   }, [stockList]);
 
   const handleResultSelect = (e, data) => {
-    dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title });
-    console.log(data.result.id)
-    history.push('/detail/' + data.result.id)
+    const selected_stock = data.result;
+    var retrieve_list = JSON.parse(localStorage.getItem('recent-search')) || [];
+    console.log(retrieve_list);
+    
+    if(retrieve_list.length === 0
+       || !retrieve_list.find(element => element.id === selected_stock.id)) {
+      selected_stock.key = keyGenerator();
+      console.log(selected_stock);
+      retrieve_list.push(selected_stock);
+      localStorage.setItem('recent-search', JSON.stringify(retrieve_list));
+    }
+    
+    dispatch({ type: 'UPDATE_SELECTION', selection: selected_stock.title });
+    //history.push('/detail/' + data.result.id)
 
   }
   return (
