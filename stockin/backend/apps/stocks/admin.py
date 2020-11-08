904e3c950a321336c83ef2c22d218f2ab678157e
DOWN-LEE <daun7137@gmail.com>
2020-11-08 14:04:43 +0900

    add some fix

--- stockin/backend/apps/stocks/admin.py
+++ stockin/backend/apps/stocks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Stock,StockHistory
+from .models import Stock,StockHistory,FinancialStat
 # Register your models here.
 
 admin.site.register(Stock)
 admin.site.register(StockHistory)
+admin.site.register(FinancialStat)
