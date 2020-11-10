95aa8d3fdd50fe92e14681499c7ee2c2fbb10994
DOWN-LEE <59424336+DOWN-LEE@users.noreply.github.com>
2020-11-11 03:58:15 +0900

    edit error

--- stockin/backend/apps/Crawlers/StockCrawler.py
+++ stockin/backend/apps/Crawlers/StockCrawler.py
@@ -1,6 +1,6 @@
 import os,sys, getopt
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockin.settings')
