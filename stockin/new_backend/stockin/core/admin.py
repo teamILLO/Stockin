a6adb6408102b1ed6b2238d701162eae077567d5
lgm0905 <lgm0905@snu.ac.kr>
2020-11-13 21:50:34 +0900

    Converged models with sqlite3

---
+++ stockin/new_backend/stockin/core/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser, Stock, StockHistory, FinancialStat, News, Group, Comment
+
+
+# User admin
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'nickname', 'is_staff', 'is_active',)
+    list_filter = ('email', 'nickname', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'nickname', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email','nickname', )
+    ordering = ('email','nickname', )
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'code', 'sector', 'price',)
+    list_display_links = ('title',)
+    list_editable = ()
+    list_per_page = 100
+    list_filter = ()
+
+
+@admin.register(FinancialStat)
+class FinancialStatAdmin(admin.ModelAdmin):
+    list_display = ('id','stock', 'quarter',)
+    list_display_links = ('stock',)
+    list_editable = ()
+    list_per_page = 100
+    list_filter = ()
+
+admin.site.register(StockHistory)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_stock_id', 'stock', 'title', 'date',)
+    list_display_links = ('title',)
+    list_editable = ()
+    list_per_page = 50
+    list_filter = ()
+
+    def get_stock_id(self, obj):
+        return obj.stock.id
+
+    get_stock_id.admin_order_field  = 'stock'  #Allows column order sorting
+    get_stock_id.short_description = 'Stock ID'  #Renames column head
+
+
+admin.site.register(Group)
+
+
+admin.site.register(Comment)
+
+
---
+++ stockin/new_backend/stockin/core/forms.py
@@ -0,0 +1,19 @@
+# This file is for custom user settings.
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email','nickname',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email','nickname',)
