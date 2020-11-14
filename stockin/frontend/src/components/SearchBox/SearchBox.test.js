5b788163fbf30a8534ee795db3c7337868a8edbd
DOWN-LEE <59424336+DOWN-LEE@users.noreply.github.com>
2020-11-14 16:49:04 +0900

    remove console error

--- stockin/frontend/src/components/SearchBox/SearchBox.test.js
+++ stockin/frontend/src/components/SearchBox/SearchBox.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, screen, fireEvent, waitForElement } from '@testing-library/react';
+import { render, screen, fireEvent, waitForElement, wait } from '@testing-library/react';
 import SearchBox from './SearchBox';
 import { history } from '../../store/store';
 import { Provider } from 'react-redux';
 import { getMockStore } from '../../test-utils/mocks';
-import axios from 'axios';
+import { api } from '../../api/index';
+import { timeDay } from 'd3-time';
 
 // const localStorageMock = {
 //   getItem: jest.fn(a=>{
@@ -34,7 +35,7 @@ const mockStore = getMockStore(initialAuthState, initialStockState);
 
 describe('<SearchBox />', () => {
   let searchbox;
-  jest.useFakeTimers();
+
 
   beforeEach(() => {
     searchbox = (
@@ -45,7 +46,7 @@ describe('<SearchBox />', () => {
 
     jest.spyOn(history, 'push').mockImplementation((text) => true);
 
-    jest.spyOn(axios, 'get').mockImplementation(() => {
+    jest.spyOn(api, 'get').mockImplementation(() => {
       const response = {
         status: 200,
         data: stubStock,
@@ -58,86 +59,43 @@ describe('<SearchBox />', () => {
     jest.clearAllMocks();
   });
 
-  it('it should render without errors', () => {
-    const { getByText, unmount, debug } = render(searchbox);
-    const query = document.body.querySelector('.prompt');
-    fireEvent.change(query, { target: { value: 'foo1_title' } });
-    screen.localStorage = { setItem: jest.fn(() => [{ id: 1 }]) };
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText('foo1_title'));
-    }, 3000);
-    jest.runAllTimers();
-    // expect(text).toHaveTextContent('');
-
-    fireEvent.change(query, { target: { value: '' } });
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText(''));
-    }, 3000);
-    jest.runAllTimers();
-
-    fireEvent.change(query, { target: { value: 'foo1_title' } });
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText('foo1_title'));
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      fireEvent.keyDown(query, { key: 'ArrowDown', code: 'ArrowDown' });
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      fireEvent.keyDown(query, { key: 'Enter', code: 'Enter' });
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText(''));
-    }, 3000);
-    jest.runAllTimers();
-
-    fireEvent.change(query, { target: { value: 'foo2_title' } });
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText('foo2_title'));
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      fireEvent.keyDown(query, { key: 'ArrowDown', code: 'ArrowDown' });
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      fireEvent.keyDown(query, { key: 'Enter', code: 'Enter' });
-    }, 3000);
-    jest.runAllTimers();
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      waitForElement(() => getByText(''));
-    }, 3000);
-    jest.runAllTimers();
-
-    unmount();
-    debug();
-  });
-
   it('it should unmount', async () => {
     const { unmount } = render(searchbox);
 
     unmount();
   });
+
+  it('it should render without errors', async () => {
+    const { getByText } = render(searchbox);
+    const query = document.body.querySelector('.prompt');
+    
+    fireEvent.change(query, { target: { value: 'foo1_title' } });
+
+    await jest.useFakeTimers();
+    
+    fireEvent.change(query, { target: { value: '' } });
+    waitForElement(() => getByText(''));
+    await jest.runAllTimers();
+    
+    fireEvent.change(query, { target: { value: 'foo1_title' } });
+
+    await jest.useFakeTimers();
+    setTimeout(() => {
+      fireEvent.keyDown(query, { key: 'ArrowDown', code: 'ArrowDown' });
+      fireEvent.keyDown(query, { key: 'Enter', code: 'Enter' });
+    }, 3000);
+    await jest.runAllTimers();
+
+    fireEvent.change(query, { target: { value: 'foo2_title' } });
+
+    await jest.useFakeTimers();
+    setTimeout(() => {
+      fireEvent.keyDown(query, { key: 'ArrowDown', code: 'ArrowDown' });
+      fireEvent.keyDown(query, { key: 'Enter', code: 'Enter' });
+    }, 3000);
+    await jest.runAllTimers();
+
+  });
+
+  
 });
