31d292ef7a12fff0fbf094634bdb96d26d1911f4
lgm0905 <lgm0905@snu.ac.kr>
2020-11-27 14:55:40 +0900

    Hide password from settings.py

--- stockin/backend/stockin/settings.py
+++ stockin/backend/stockin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +114,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = 'stockinswpp@gmail.com'
-EMAIL_HOST_PASSWORD = 'swpp2020'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
