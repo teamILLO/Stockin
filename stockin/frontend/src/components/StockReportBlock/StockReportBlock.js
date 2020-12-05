0613f8251ee3566b805616df3f46d4372fe80145
lgm0905 <lgm0905@snu.ac.kr>
2020-12-06 02:23:40 +0900

    mid commit to stockblock(don't care about this)

--- stockin/frontend/src/components/StockReportBlock/StockReportBlock.js
+++ stockin/frontend/src/components/StockReportBlock/StockReportBlock.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Segment } from 'semantic-ui-react'
+import { Segment, Header, Grid, Item, Image, Statistic } from 'semantic-ui-react'
 import { api } from '../../api/index';
 import { timeParse } from 'd3-time-format';
 
@@ -8,8 +8,52 @@ const StockReportBlock = (props) => {
   const title = props.title;
   const rank = props.rank;
   const info = (props.isKOSPI ? 'KOSPI' : 'KOSDAQ') + ' ' + props.code;
+  const score = props.score;
   const price = props.price;
   const variation = (props.price + 0) > (props.yesterdayPrice + 0) ? props.price - props.yesterdayPrice : props.yesterdayPrice - props.price;
+
+  const rankComponent =
+    <Segment>
+      <Header as='h1' color='red'>{rank + '위'}</Header>
+    </Segment>;
+
+  const titleComponent = 
+  <Segment>
+    <div>
+      <Header as='h1'>
+        {title}
+      </Header>
+      <Header as='h1'>
+        {info}
+      </Header>
+      <Header as='h2' color='grey'>
+        현재가 : 10000원
+      </Header>
+    </div>
+  </Segment>
+
+  const scoreComponent = 
+  <Segment>
+    <Statistic horizontal label='점' value={score + '/100'} />
+  </Segment>;
+
+  const graphComponent = 
+  <Segment>
+    <Header as='h1'>
+        그래프
+      </Header>
+    <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+  </Segment>;
+
+  const newsComponent = 
+  <Segment>
+    <div>
+      <Header as='h1'>
+        주목할만한 뉴스
+      </Header>
+      <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />;
+    </div>
+  </Segment>;
 
   /* TODO : FOR GRAPH */
   //const priceList
@@ -31,17 +75,33 @@ const StockReportBlock = (props) => {
   
   // let graph = priceList  === '' ? 'Loading...' : <DetailData data={priceList} />;
 
+  
+
   return(
     <Segment color='red' data-testid='StockReportBlock'>
-        <div>rank: {props.rank}</div> 
-        <div>{title}</div>
-        <div>{info}</div>
-        <div>{price} {variation}</div>
-        {/* {graph} */}
-        
-    </Segment>
+      <Grid divided='vertically'>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            {rankComponent}
+          </Grid.Column>
+          <Grid.Column>
+            {titleComponent}
+          </Grid.Column>
+          <Grid.Column>
+            {scoreComponent}
+          </Grid.Column>
+        </Grid.Row>
 
-   
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            {graphComponent}
+          </Grid.Column>
+          <Grid.Column>
+            {newsComponent}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
