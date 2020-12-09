287352e9d223d4b4cb1995198954ef58e044788f
Wooyoung Choi <w980417@gmail.com>
2020-12-10 07:55:45 +0900

    edit login view

--- stockin/frontend/src/containers/PreloginPage/PreloginPage.js
+++ stockin/frontend/src/containers/PreloginPage/PreloginPage.js
@@ -9,6 +9,7 @@ import { tryLogin, checkLogin } from '../../store/authentication/authentication'
 import SignupModal from '../../components/Modal/SignupModal/SignupModal';
 import FindPasswdModal from '../../components/Modal/FindPasswdModal/FindPasswdModal';
 import { api } from '../../api/index';
+import './PreloginPage.css';
 
 const panes = [
   {
@@ -60,8 +61,6 @@ const PreloginPage = (props) => {
 
   return (
     <div className="PreloginPage" data-testid="PreloginPage">
-      Prelogin Page
-      <br />
       <div>
         <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
           <Grid.Column width={7}>
@@ -86,20 +85,27 @@ const PreloginPage = (props) => {
                   value={password}
                   onChange={(event) => setPassword(event.target.value)}
                 />
-
-                <Checkbox label="Remember Me" />
-                <FindPasswdModal trigger={<Item as="a" content="Forget Password?" />} />
-
-                <Button className="mainButton" size="large" onClick={submitHandler}>
-                  Login
-                </Button>
-                <SignupModal
-                  trigger={
-                    <Button className="mainBasicButton" size="large">
-                      Sign up
-                    </Button>
-                  }
-                />
+                <div>
+                  <Checkbox label="Remember Me" className="rememberMe" />
+                  <FindPasswdModal
+                    trigger={<Item as="a" content="Forget Password?" className="forgetPassword" />}
+                  />
+                </div>
+                <br />
+                <br />
+                <br />
+                <div>
+                  <Button className="mainButton" size="large" onClick={submitHandler}>
+                    Login
+                  </Button>
+                  <SignupModal
+                    trigger={
+                      <Button className="mainBasicButton" size="large">
+                        Sign up
+                      </Button>
+                    }
+                  />
+                </div>
               </Form>
             </Grid.Column>
           </Grid.Column>
