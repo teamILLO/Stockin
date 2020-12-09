ffefb1cdf80a1d26854acc7a31c79c1b965a27a6
lgm0905 <lgm0905@snu.ac.kr>
2020-12-09 12:44:01 +0900

    Change color of graph, little bit BE

--- stockin/frontend/src/components/StockReportBlock/GraphComponent.js
+++ stockin/frontend/src/components/StockReportBlock/GraphComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip
   } from 'recharts';
 
 const GraphComponent = (props) => {
