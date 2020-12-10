3a27306264ffa719fc096c59373c31b49efd833a
Junhyeok Park <dillonzzang@gmail.com>
2020-12-10 10:41:12 +0900

    added preview tab on prelogin page

--- stockin/frontend/src/components/Preview/Preview.js
+++ stockin/frontend/src/components/Preview/Preview.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Grid, Header, Image } from 'semantic-ui-react';
+import my from '../../images/preview_mypage.png';
+import main from '../../images/preview_mainpage.png';
 
 const Preview = (props) => {
-  return <div data-testid="Preview">{/* Here comes the content of Preview Tab*/}Preview tab</div>;
+  return (
+    <div data-testid="Preview">
+      <br />
+      <Grid>
+        <Header as="h4">Efficiently ordered stocks</Header>
+        <Image src={main} size="large" centered />
+        <Header as="h4">Comparison by groups</Header>
+        <Image src={my} size="large" centered />
+      </Grid>
+    </div>
+  );
 };
 
 export default Preview;
