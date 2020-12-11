fb40f87d6f424640ef794749b381b21c2fdcd339
Junhyeok Park <dillonzzang@gmail.com>
2020-12-12 03:51:34 +0900

    SignupModal 100% covered

--- stockin/frontend/src/components/Modal/SignupModal/SignupModal.test.js
+++ stockin/frontend/src/components/Modal/SignupModal/SignupModal.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, wait} from '@testing-library/react';
+import { render, screen, fireEvent, wait } from '@testing-library/react';
 import SignupModal from './SignupModal';
 import store from '../../../store/store';
 import { Provider } from 'react-redux';
@@ -17,20 +17,19 @@ describe('<SignupModal />', () => {
 
     spyPost = jest.spyOn(api, 'post').mockImplementation((url, atc) => {
       return new Promise((resolve, reject) => {
-          let result
-          if(url === '/users/duplicate/')
-              result = {
-                  status: 200,
-                  data: {'duplicate': false},
-              };
-          else
-              result = {
-                  status: 200,
-              }
-          resolve(result);
+        let result;
+        if (url === '/users/duplicate/')
+          result = {
+            status: 200,
+            data: { duplicate: false },
+          };
+        else
+          result = {
+            status: 200,
+          };
+        resolve(result);
       });
     });
-
   });
 
   afterEach(() => {
@@ -68,6 +67,41 @@ describe('<SignupModal />', () => {
     expect(query.value).toBe('password');
   });
 
+  test(`should show error on invalid email input`, () => {
+    render(signup);
+    fireEvent.click(screen.getAllByText(/trigger/i, { selector: 'button' })[0]);
+    const query1 = screen.getByPlaceholderText('Email');
+    fireEvent.change(query1, { target: { value: 'a' } });
+    const query2 = screen.getByRole('button', { name: /next/i });
+    fireEvent.click(query2);
+    const query3 = screen.queryAllByRole('alert');
+    expect(query3.length).toBe(1);
+  });
+
+  test(`should change value of email on invalid email input`, () => {
+    render(signup);
+    fireEvent.click(screen.getAllByText(/trigger/i, { selector: 'button' })[0]);
+    const query1 = screen.getByPlaceholderText('Email');
+    fireEvent.change(query1, { target: { value: 'a' } });
+    const query2 = screen.getByRole('button', { name: /next/i });
+    fireEvent.click(query2);
+    const query3 = screen.getByPlaceholderText('Email');
+    fireEvent.change(query3, { target: { value: 'test@email.com' } });
+    expect(query3.value).toBe('test@email.com');
+  });
+
+  test(`should change value of password on invalid email input`, () => {
+    render(signup);
+    fireEvent.click(screen.getByText(/trigger/i, { selector: 'button' }));
+    const query1 = screen.getByPlaceholderText('Email');
+    fireEvent.change(query1, { target: { value: 'a' } });
+    const query2 = screen.getByRole('button', { name: /next/i });
+    fireEvent.click(query2);
+    const query3 = screen.getByPlaceholderText('Password');
+    fireEvent.change(query3, { target: { value: 'password' } });
+    expect(query3.value).toBe('password');
+  });
+
   test(`should change value in nickname input field`, async () => {
     render(signup);
     fireEvent.click(screen.getByText(/trigger/i, { selector: 'button' }));
@@ -75,29 +109,27 @@ describe('<SignupModal />', () => {
     fireEvent.change(email, { target: { value: 'test@email.com' } });
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(password, { target: { value: 'password' } });
-    await wait(() =>
-      fireEvent.click(screen.getByText(/Next/i, { selector: 'button' }))
-    );
+    await wait(() => fireEvent.click(screen.getByText(/Next/i, { selector: 'button' })));
     const query = screen.getByPlaceholderText('Nickname');
     fireEvent.change(query, { target: { value: 'nickname' } });
     expect(query.value).toBe('nickname');
   });
 
   test(`should show alert when duplicated email`, async () => {
-    window.alert =jest.fn();
+    window.alert = jest.fn();
     spyPost = jest.spyOn(api, 'post').mockImplementation((url, atc) => {
       return new Promise((resolve, reject) => {
-          let result
-          if(url === '/users/duplicate/')
-              result = {
-                  status: 200,
-                  data: {'duplicate': true},
-              };
-          else
-              result = {
-                  status: 200,
-              }
-          resolve(result);
+        let result;
+        if (url === '/users/duplicate/')
+          result = {
+            status: 200,
+            data: { duplicate: true },
+          };
+        else
+          result = {
+            status: 200,
+          };
+        resolve(result);
       });
     });
     render(signup);
@@ -106,47 +138,41 @@ describe('<SignupModal />', () => {
     fireEvent.change(email, { target: { value: 'test@email.com' } });
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(password, { target: { value: 'password' } });
-    await wait(() =>
-      fireEvent.click(screen.getByText(/Next/i, { selector: 'button' }))
-    );
+    await wait(() => fireEvent.click(screen.getByText(/Next/i, { selector: 'button' })));
     const query = screen.getByPlaceholderText('Email');
     fireEvent.change(query, { target: { value: 'test@email.com' } });
     expect(query.value).toBe('test@email.com');
   });
 
   test(`should show alert when duplicated nickname`, async () => {
-    window.alert =jest.fn();
+    window.alert = jest.fn();
     render(signup);
     fireEvent.click(screen.getByText(/trigger/i, { selector: 'button' }));
     const email = screen.getByPlaceholderText('Email');
     fireEvent.change(email, { target: { value: 'test@email.com' } });
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(password, { target: { value: 'password' } });
-    await wait(() =>
-      fireEvent.click(screen.getByText(/next/i, { selector: 'button' }))
-    );
+    await wait(() => fireEvent.click(screen.getByText(/next/i, { selector: 'button' })));
 
     spyPost = jest.spyOn(api, 'post').mockImplementation((url, atc) => {
       return new Promise((resolve, reject) => {
-          let result
-          if(url === '/users/duplicate/')
-              result = {
-                  status: 200,
-                  data: {'duplicate': true},
-              };
-          else
-              result = {
-                  status: 200,
-              }
-          resolve(result);
+        let result;
+        if (url === '/users/duplicate/')
+          result = {
+            status: 200,
+            data: { duplicate: true },
+          };
+        else
+          result = {
+            status: 200,
+          };
+        resolve(result);
       });
     });
-    
+
     const nickname = screen.getByPlaceholderText('Nickname');
     fireEvent.change(nickname, { target: { value: 'nickname' } });
-    await wait(() =>
-      fireEvent.click(screen.getByText(/sign up/i, { selector: 'button' }))
-    );
+    await wait(() => fireEvent.click(screen.getByText(/sign up/i, { selector: 'button' })));
     const query = screen.getByPlaceholderText('Nickname');
     fireEvent.change(query, { target: { value: 'nickname' } });
     expect(query.value).toBe('nickname');
@@ -160,9 +186,7 @@ describe('<SignupModal />', () => {
     fireEvent.change(email, { target: { value: 'test@email.com' } });
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(password, { target: { value: 'password' } });
-    await wait(() =>
-      fireEvent.click(screen.getByText(/Next/i, { selector: 'button' }))
-    );
+    await wait(() => fireEvent.click(screen.getByText(/Next/i, { selector: 'button' })));
     const nickname = screen.getByPlaceholderText('Nickname');
     fireEvent.change(nickname, { target: { value: 'nickname' } });
     fireEvent.click(screen.getByText(/sign up/i, { selector: 'button' }));
