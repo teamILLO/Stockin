111518368af00222e5ed7792a8b3ee9bc811203d
Junhyeok Park <dillonzzang@gmail.com>
2020-12-12 03:27:19 +0900

    DetailOverview 100% covered

--- stockin/frontend/src/components/Detail/DetailOverview/DetailOverview.test.js
+++ stockin/frontend/src/components/Detail/DetailOverview/DetailOverview.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { render, queryAllByTestId } from '@testing-library/react';
+import { render, queryAllByTestId, queryAllByText } from '@testing-library/react';
 import DetailOverview from './DetailOverview';
 
 describe('<DetailOverview />', () => {
   it('should render without errors', () => {
     const { container } = render(<DetailOverview />);
+    const query = queryAllByTestId(container, 'DetailOverview');
+    expect(query.length).toBe(1);
+  });
+  it('should render without errors', () => {
+    const { container } = render(<DetailOverview stock={{ score: 2 }} />);
+    const query = queryAllByTestId(container, 'DetailOverview');
+    expect(query.length).toBe(1);
+  });
+  it('should render without errors', () => {
+    const { container } = render(
+      <DetailOverview stock={{ score: 2 }} fs_score={{ score: undefined }} />,
+    );
+    const query = queryAllByTestId(container, 'DetailOverview');
+    expect(query.length).toBe(1);
+  });
+  it('should render without errors', () => {
+    const { container } = render(
+      <DetailOverview stock={{ score: 61 }} fs_score={{ score: 2.5 }} />,
+    );
     const query = queryAllByTestId(container, 'DetailOverview');
     expect(query.length).toBe(1);
   });
